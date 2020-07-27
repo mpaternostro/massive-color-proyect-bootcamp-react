eee2750fd7d5aa76a1f1f4d3d9cb6b41107fc21b
mpaternostro <marcelodpaternostro@gmail.com>
2020-07-27 10:26:41 -0300

    Updates

---
+++ precache-manifest.57258c67ff57b9601e127d7a0ebaea65.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de6fb56f89a8e18f2070c5bbe7a936f8",
+    "url": "/react-color-picker/index.html"
+  },
+  {
+    "revision": "a386b9a26396a41c3e9e",
+    "url": "/react-color-picker/static/css/2.2a4460b4.chunk.css"
+  },
+  {
+    "revision": "e0d16128e413675f8811",
+    "url": "/react-color-picker/static/css/main.a2ac2a70.chunk.css"
+  },
+  {
+    "revision": "a386b9a26396a41c3e9e",
+    "url": "/react-color-picker/static/js/2.d1eb1585.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/react-color-picker/static/js/2.d1eb1585.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0d16128e413675f8811",
+    "url": "/react-color-picker/static/js/main.7f112d23.chunk.js"
+  },
+  {
+    "revision": "e12d0f17b117823751a5",
+    "url": "/react-color-picker/static/js/runtime-main.103c7576.js"
+  },
+  {
+    "revision": "9b4f3912876bbcb8b56433a69b24ffcc",
+    "url": "/react-color-picker/static/media/bg.9b4f3912.svg"
+  }
+]);
--- src/styles/DraggableColorListStyles.js
+++
@@ -1,7 +0,0 @@
-export default {
-  currentPaletteColors: {
-    height: "calc(100vh - 64px)",
-    margin: "0",
-    padding: "0",
-  },
-};
